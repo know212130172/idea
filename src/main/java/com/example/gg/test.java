fbf0900f2a02a954d168d236ac33f9a3dcfc6d89
123 <123@qq.com>
2019-11-02 12:10:30 +0800

    IDEA

---
+++ src/main/java/com/example/gg/test.java
@@ -0,0 +1,18 @@
+package com.example.gg;
+
+
+
+public class test {
+          private String name;    
+          private String sex;     
+          private Integer age;    
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
